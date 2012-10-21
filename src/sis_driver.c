10e335c14aa730872d3c59ee3d66b78dcbd104df
AlexanderR <alexander.r@gmx.com>
2012-10-21 15:17:44 +1100

    Remove mibstore.h

--- src/sis_driver.c
+++ src/sis_driver.c
@@ -86,7 +86,6 @@
 #include "fb.h"
 #include "micmap.h"
 #include "mipointer.h"
-#include "mibstore.h"
 #include "edid.h"
 
 #define SIS_NEED_inSISREG
@@ -9652,7 +9651,6 @@ SISScreenInit(int scrnIndex, ScreenPtr pScreen, int argc, char **argv)
     }
     pSiS->SiSFastVidCopyDone = TRUE;
 
-    miInitializeBackingStore(pScreen);
     xf86SetBackingStore(pScreen);
     xf86SetSilkenMouse(pScreen);
 
